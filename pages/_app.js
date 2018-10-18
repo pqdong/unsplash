566407b5898514723b6e217ebbb5b25c2130d0fd
Dong Phung <dong.phung@tiki.vn>
2018-10-18 15:56:24 +0700

    feat(Photo list): Updating

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,12 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import withRedux from 'next-redux-wrapper'
 import withReduxSaga from 'next-redux-saga'
+import { PageTransition } from 'next-page-transitions'
+import Spinner from '../components/Spinner'
 
 import createStore from '../store'
+
+const TIMEOUT = 300
 
 class Unsplash extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -23,8 +27,48 @@ class Unsplash extends App {
     return (
       <Container>
         <Provider store={store}>
-          <Component {...pageProps} />
+          <PageTransition
+            timeout={TIMEOUT}
+            classNames="page-transition"
+            loadingComponent={Spinner}
+            loadingDelay={500}
+            loadingTimeout={{
+              enter: TIMEOUT,
+              exit: 0,
+            }}
+            loadingClassnames="loading-indicator"
+          >
+            <Component {...pageProps} />
+          </PageTransition>
         </Provider>
+
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+            transform: translate3d(0, 20px, 0);
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transform: translate3d(0, 0, 0);
+            transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity ${TIMEOUT}ms;
+          }
+          .loading-indicator-appear,
+          .loading-indicator-enter {
+            opacity: 0;
+          }
+          .loading-indicator-appear-active,
+          .loading-indicator-enter-active {
+            opacity: 1;
+            transition: opacity ${TIMEOUT}ms;
+          }
+        `}</style>
       </Container>
     )
   }
